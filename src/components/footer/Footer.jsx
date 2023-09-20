71c72bbcac91d07b736060d30772fad510d1f227
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2023-09-21 00:43:42 +0700

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Verga</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.facebook.com/verga.v3r"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="bx bxl-facebook"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/tandika10"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
+          <a href="#footer" className="footer__social-link">
+            <i className="bx bxl-twitter"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">
+          &#169; Verga.dev. All rigths reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/tandika10"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.dribbble.com/tandika10"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-dribbble"></i>
+      </a>
+      <a
+        href="https://www.github.com/vergatan10"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
